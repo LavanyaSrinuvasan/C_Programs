fce019f3350aadf42a04938e17bdcb5ee51f5a3d
LavanyaSrinuvasan <107125097+LavanyaSrinuvasan@users.noreply.github.com>
2023-08-24 15:49:00 +0530

    Update p67.c

--- p67.c
+++ p67.c
@@ -16,26 +16,51 @@ int main()
     if(units<=199)
     {
         amount=units*1.2;
+        printf("The amount is %f\n",amount);
     }
+
     else if ((units>=200)&&(units<400))
     {
-        amount=(199*1.2)+(units-199)*1.5;
+        amount=(199*1.2)+((units-199)*1.5);
+        if(amount>=400)
+    {
+    amount=amount*1.15;
+    printf("The surcharge added bill the customer should pay is %.2f\n",amount);
+    }
+        else
+        {
+            printf("The amount is %.2f",amount);
+        }
     }
 
     else if ((units>=400)&&(units<600))
     {
-        amount=(199*1.2)+(200*1.5)+(units-399)*1.8;
+        amount=(199*1.2)+(199*1.5)+((units-399)*1.8);
+        if(amount>=400)
+    {
+    amount=amount*1.15;
+    printf("The surcharge added bill the customer should pay is %.2f\n",amount);
+    }
+        else
+        {
+            printf("The amount is %.2f",amount);
+        }
     }
     else if(units>=600)
     {
-        amount=(199*1.2)+(200*1.5)+(200*1.8)+(units-599)*2;
-    }
-    printf("The Electricity bill is %.2f\n",amount);
-    if(amount>=400)
+        amount=(199*1.2)+(199*1.5)+(199*1.8)+((units-599)*2);
+        if(amount>=400)
     {
-    amount=amount+0.15;
+    amount=amount*1.15;
     printf("The surcharge added bill the customer should pay is %.2f\n",amount);
     }
+        else
+        {
+            printf("The amount is %.2f",amount);
+        }
+    }
+   // printf("The Electricity bill is %.2f\n",amount);
+
     else if(amount<=100)
     {
     printf("The base electricity bill the customer should pay is Rs. %d",base);
