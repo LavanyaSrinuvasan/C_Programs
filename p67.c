8229b0bdb3b32c2e8549b7266d90f9f651d08b37
LavanyaSrinuvasan <107125097+LavanyaSrinuvasan@users.noreply.github.com>
2023-08-24 14:55:42 +0530

    Add files via upload

--- p67.c
+++ p67.c
@@ -15,22 +15,23 @@ int main()
 
     if(units<=199)
     {
-        amount=units*1.20;
+        amount=units*1.2;
     }
     else if ((units>=200)&&(units<400))
     {
-        amount=units*1.50;
+        amount=(199*1.2)+(units-199)*1.5;
     }
+
     else if ((units>=400)&&(units<600))
     {
-        amount=units*1.80;
+        amount=(199*1.2)+(200*1.5)+(units-399)*1.8;
     }
     else if(units>=600)
     {
-        amount=units*2.00;
+        amount=(199*1.2)+(200*1.5)+(200*1.8)+(units-599)*2;
     }
     printf("The Electricity bill is %.2f\n",amount);
-    if(amount>400)
+    if(amount>=400)
     {
     amount=amount+400;
     printf("The surcharge added bill the customer should pay is %.2f\n",amount);
